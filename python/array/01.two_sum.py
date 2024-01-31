4b3c382009a434082025a329716c5c6442d71e6c
deepakstomar <deepaktomar.tech@gmail.com>
2024-01-31 12:06:25 +0530

    Solved using hash map

---
+++ python/array/01.two_sum.py
@@ -0,0 +1,15 @@
+
+def two_sum(nums, target):
+    nums_dict = {}
+    n = len(nums)
+
+    for i in range(n):
+        difference = target - nums[i]
+        if difference in nums_dict:
+            return [nums_dict[difference], i]
+        nums_dict[nums[i]] = i
+
+nums = [2,3,5,6]
+target = 7
+
+print(two_sum(nums, target))
