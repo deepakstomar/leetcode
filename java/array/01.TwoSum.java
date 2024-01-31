3208e1cfe549d25f5e83a582eadeb5bd99229e10
deepakstomar <deepaktomar.tech@gmail.com>
2024-01-31 12:07:05 +0530

    Solved using brute force

---
+++ java/array/01.TwoSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (result == nums[j]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
